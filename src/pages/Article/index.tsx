a513b229bf7a037beecc569289b8ce23f922a4ed
yeah1997 <lee7778889@gmail.com>
2022-09-22 16:12:47 +0900

    render Article component

--- src/pages/Article/index.tsx
+++ src/pages/Article/index.tsx
@@ -2,11 +2,14 @@ import Icon from '@/components/Icon'
 import NavBar from '@/components/NavBar'
 import { useEffect } from 'react'
 // import ContentLoader from 'react-content-loader'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useHistory, useParams } from 'react-router'
 import styles from './index.module.scss'
 
 import { getArticleDetail } from '@/store/actions/article'
+import { RootState } from '@/store'
+import classNames from 'classnames'
+import dayjs from 'dayjs'
 
 const Article = () => {
   // history
@@ -14,6 +17,8 @@ const Article = () => {
 
   // dispatch
   const dispatch = useDispatch()
+
+  const detail = useSelector((state: RootState) => state.article.detail)
 
   const { id } = useParams<{ id: string }>()
 
@@ -36,6 +41,45 @@ const Article = () => {
         >
           div
         </NavBar>
+        <>
+          <div className="wrapper">
+            <div className="article-wrapper">
+              {/* 文章描述信息栏 */}
+              <div className="header">
+                <h1 className="title">{detail.title}</h1>
+
+                <div className="info">
+                  <span>{dayjs(detail.pubdate).format('YYYY-MM-DD')}</span>
+                  <span>{detail.read_count} 阅读</span>
+                  <span>{detail.comm_count} 评论</span>
+                </div>
+
+                <div className="author">
+                  <img src={detail.aut_photo} alt="" />
+                  <span className="name">{detail.aut_name}</span>
+                  <span
+                    className={classNames('follow', {
+                      followed: detail.is_followed,
+                    })}
+                  >
+                    {detail.is_followed ? '已关注' : '关注'}
+                  </span>
+                </div>
+              </div>
+
+              {/* 文章正文内容区域 */}
+              <div className="content">
+                <div
+                  className="content-html dg-html"
+                  dangerouslySetInnerHTML={{ __html: detail.content }}
+                ></div>
+                <div className="date">
+                  发布文章时间：{dayjs(detail.pubdate).format('YYYY-MM-DD')}
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
       </div>
     </div>
   )
